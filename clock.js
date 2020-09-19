33c3355e7d8604a5f6008768946dd3f8d6e63a87
Sahar12j14 <71217328+Sahar12j14@users.noreply.github.com>
2020-09-20 01:16:53 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,18 @@
+function myTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  var str = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clock").innerHTML = str;
+}
+setInterval(myTime, 1000);
